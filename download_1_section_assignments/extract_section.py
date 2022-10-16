61a53242e318fe1aebb61e0e81fd1f191bf3a8c2
Elliott <ecmagnuson@wisc.edu>
2022-10-16 00:21:31 -0500

    added rename to files WIP

--- download_1_section_assignments/extract_section.py
+++ download_1_section_assignments/extract_section.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python3
+
 from __future__ import annotations
 from os.path import dirname, abspath
 import os
 import shutil
 
-def get_students():
+'''
+reads the students from students.txt and extracts their assignments from
+the submissions directory into the wanted_submissions directory.
+'''
+
+def get_students() -> Lst[str]:
     '''
     Returns a list of the students (format 'lastnamefirstname') from student.txt
     after removing upper case, commas, and the middle name from each student
@@ -19,13 +26,43 @@ def get_students():
                 pass
     return students
 
-def extract_submission():
+def make_dir(dir: str) -> None:
+    '''Makes the directory if it doesnt exist '''
+    if os.path.isdir(dir):
+        pass
+    else:
+        os.mkdir(dir)
+
+def rename_files():
+    submissions = dirname(abspath(__file__)) + '/wanted_submissions/'
+    student_files = os.listdir(submissions)
+
+    for file in student_files:
+        old_file = os.path.join(submissions, file)
+        new_file = os.path.join(submissions, file[ : file.index('_')] + 'ProposalPaperGraded')
+        os.rename(old_file, new_file)
+
+def extract_submissions() -> None:
+    '''
+    extracts all of the files from the submissions directory to a new directory 
+    called 'wanted_submissions'
+    '''
     submissions = dirname(abspath(__file__)) + '/submissions/'
     destination = dirname(abspath(__file__)) + '/wanted_submissions/'
-    for file in os.listdir(submissions):
-        for student in get_students():
-            if student in file:
-                shutil.copyfile(submissions + file, destination + file)
+    student_files = os.listdir(submissions)
+    students = get_students()
+    make_dir(destination)
+
+    for i, sfile in enumerate(student_files):
+        for student in students:
+            if student in sfile:
+                shutil.copyfile(submissions + sfile, destination + sfile)
+
+def main():
+    extract_submissions()
+    rename_files()
 
 if __name__ == '__main__':
-    extract_submission()
+    main()
+    
+    
