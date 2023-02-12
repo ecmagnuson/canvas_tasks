266e2bcf0b53a707602705bac37b66aa0584bc29
Elliott Magnuson <ecmagnuson@wisc.edu>
2023-02-11 22:29:02 -0600

    Exception handling for invalid user response

--- python/GetStudentSubmissions/get_assingments.py
+++ python/GetStudentSubmissions/get_assingments.py
@@ -21,7 +21,7 @@ def validate():
         API_KEY = d["API_KEY"]
         return Canvas(API_URL, API_KEY)
     except KeyError:
-        print('There was an error accessing the API_URL or API_KEY inside of the "Auth.json" file')
+        print('There was an error accessing the API_URL or API_KEY inside of the "Auth.json" file.')
         print("Enter each inside of double quotes, i.e.")
         print('"https://canvas.wisc.edu/"')
         sys.exit(1)
@@ -29,18 +29,19 @@ def validate():
 def desired_course(canvas):
     #return a Course object that the user picked
     courses = active_canvas_courses(canvas)
-    print(f"There are {len(courses)} active courses in your Canvas page in which you are a teacher")
-    print("Which would you like to choose?")
+    print(f"There are {len(courses)} active courses in your Canvas page in which you are a teacher.")
     print('''Note: Some old classes may be listed here. 
-        If they are, that means they are still listed as active under Canvas - 
-        which there is nothing I can do about\n''')
+          If they are, that means they are still listed as active under Canvas. \n''')
     for i, course in enumerate(courses):
         print(f"({i}) --" , course.name)
-    try:
-        choice = int(input("> "))
-        return courses[choice]
-    except (ValueError, IndexError):
-        print("Enter a digit corresponding to the course")
+    while True:
+        try:
+            print("\nWhich class would you like to choose?")
+            choice = int(input("> "))
+            print()
+            return courses[choice]
+        except (ValueError, IndexError):
+            print("Enter a digit corresponding to the course")
 
 def active_canvas_courses(canvas):
     #return a list of all active Canvas courses which you are a ta or teacher
@@ -64,15 +65,17 @@ def active_canvas_courses(canvas):
 
 def desired_section(course):
     #return a Section object corresponding to the user input
-    print(f"What section of {course.name} do you want to get?")
     sections = course.get_sections()
     for i, section in enumerate(sections):
         print(f"({i}) --" , section)
-    try:
-        choice = int(input("> "))
-        return sections[choice]
-    except (ValueError, IndexError):
-        print("Enter a digit corresponding to the section")
+    while True:
+        try:
+            print(f"\nWhat section of {course.name} do you want to get?")
+            choice = int(input("> "))
+            print()
+            return sections[choice]
+        except (ValueError, IndexError):
+            print("Enter a digit corresponding to the section")
 
 @dataclass
 class Student: 
@@ -93,22 +96,25 @@ def populate_enrollment(section):
 def get_published_assignments(course):
     #TODO this hangs for a bit
     assignments = course.get_assignments()
-    print("What assignment do you want to download the files for?")
+    print("Please wait. Sometimes this can hang for a little while.")
     for i, assignment in enumerate(assignments):
         if assignment.published:
             print(f"({i}) --" , assignment.name)
-    try:
-        choice = int(input("> "))
-        return assignments[choice]
-    except (ValueError, IndexError):
-        print("Enter a digit corresponding to the published assignment.")
+    while True:
+        try:
+            print("\nWhat assignment do you want to download the files for?")
+            choice = int(input("> "))
+            return assignments[choice]
+        except (ValueError, IndexError):
+            print("Enter a digit corresponding to the published assignment.")
 
 def download_assignments(students, assignment):
     #downloads assignments into ./submissions directory
-    print("What do you want to call the name of the file for each student?")
+    print("\nWhat do you want to call the name of the file for each student?")
     assignment_name = input("> ")
     os.makedirs("./submissions", exist_ok=True)
     os.makedirs("./submissions/" + assignment_name, exist_ok=True)
+    print()
     for i, student in enumerate(students, 1):
         try:
             file_name = str(assignment.get_submission(student.ID).attachments[0])
